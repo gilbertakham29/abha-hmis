42a416cd8a68b011ba38229f416139c5c9e6868e
gocoin21 <gocoin21@gmail.com>
2024-03-09 11:15:58 +0530

    deleted some files

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,12 @@
-import { combineReducers } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 
 import rootReducer from "./reducer";
 type RootState = ReturnType<typeof rootReducer>;
 
-const store = combineReducers({
-  reducer: rootReducer,
+const storeReducer = combineReducers({
+  storereducer: rootReducer,
 });
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-
+const store = configureStore({ reducer: storeReducer });
 export type { RootState };
 export default store;
