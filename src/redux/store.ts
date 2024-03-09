3aad9719957bc52ef1de64b83c55264463af7365
gocoin21 <gocoin21@gmail.com>
2024-03-09 11:04:55 +0530

    make changes in reducer

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,37 +1,12 @@
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import {
-  abhaCardResultReducer,
-  abhaInitiationReducer,
-  abhaQrResultReducer,
-  adhaarVerificationOtpReducer,
-  createHealtIdReducer,
-  getConsentHeaderReducer,
-  getHealthInfoReducer,
-  mobileOtpReducer,
-  mobileVerificationOtpReducer,
-  resendAdhaarOtpReducer,
-  searchResultReducer,
-} from "./reducer";
-//import rootReducer from "./reducer";
+import { combineReducers } from "@reduxjs/toolkit";
+
+import rootReducer from "./reducer";
 type RootState = ReturnType<typeof rootReducer>;
 
-const rootReducer = combineReducers({
-  searchResultReducer,
-  abhaCardResultReducer,
-  abhaQrResultReducer,
-  abhaInitiationReducer,
-  adhaarVerificationOtpReducer,
-  resendAdhaarOtpReducer,
-  mobileOtpReducer,
-  mobileVerificationOtpReducer,
-  createHealtIdReducer,
-  getHealthInfoReducer,
-  getConsentHeaderReducer,
-});
-const store = configureStore({
+const store = combineReducers({
   reducer: rootReducer,
 });
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch;
+
 export type { RootState };
 export default store;
