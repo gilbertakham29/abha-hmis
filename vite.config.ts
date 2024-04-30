c353b7312c018fafae25b8779a136bfb528a0cec
gocoin21 <gocoin21@gmail.com>
2024-04-30 14:45:00 +0530

    changes in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "https://picasoid-abdm.azurewebsites.net",
   plugins: [react()],
   server: {
     proxy: {
